2c45e48974e3e8bb6825919d34799a72eca41bfe
Francesco Biegi <f.biegi@dirimologistika.it>
2018-02-03 09:13:31 +0100

    Initial commit

---
+++ app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thread;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    public function index()
+    {
+        $threads = Thread::latest()->get();
+        return view('threads.index', compact('threads'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+            'channel_id' => 'required|exists:channels,id'
+        ]);
+
+        $thread = Thread::create([
+            'user_id' => auth()->id(),
+            'channel_id' => request('channel_id'),
+            'title' => request('title'),
+            'body' => request('body'),
+        ]);
+
+        return redirect($thread->path());
+    }
+
+    public function show($channelId, Thread $thread)
+    {
+        return view('threads.show', compact('thread'));
+    }
+
+    public function create()
+    {
+        return view('threads.create');
+    }
+}
---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function unauthenticated_users_may_not_add_areplies()
+    {
+        $this->withExceptionHandling()
+            ->post('/threads/some-channel/1/replies', [])
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $this->be(factory('App\User')->create());
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply');
+        $this->post($thread->path() . '/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
