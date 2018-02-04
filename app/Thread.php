6ac6fb9dd26ed3c810c2ca4bfe3bd27696ed4efb
Francesco Biegi <f.biegi@dirimologistika.it>
2018-02-04 12:14:26 +0100

    lesson 21

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,8 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
     protected static function boot()
     {
         parent::boot();
@@ -34,9 +36,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function addReply($reply)
